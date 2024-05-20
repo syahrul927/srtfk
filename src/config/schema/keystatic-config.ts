0a9abdda91c87414818b3d47b4d0569ddf0f692e
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2024-05-20 14:59:07 +0700

    feat: github auth keystatic

--- src/config/schema/keystatic-config.ts
+++ src/config/schema/keystatic-config.ts
@@ -1,10 +1,19 @@
-import { config } from "@keystatic/core"
+import { config, type GitHubConfig, type LocalConfig } from "@keystatic/core"
 import PostCollection from "~/config/schema/collections/post"
-
+const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
+	process.env.NODE_ENV === "development"
+		? {
+				kind: "local",
+		  }
+		: {
+				kind: "github",
+				repo: {
+					owner: "syahrul927",
+					name: "srtfk",
+				},
+		  }
 const KeystaticConfig = config({
-	storage: {
-		kind: "local",
-	},
+	storage,
 	collections: {
 		posts: PostCollection,
 	},
