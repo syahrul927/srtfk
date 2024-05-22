9713856840e9d282e79a06ce3bfdf629b8d427d3
srtfk <syahrulataufikaja11@gmail.com>
2024-05-22 23:27:27 +0700

    feat: display publish date

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import moment from "moment"
+import "moment/locale/id"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+export function localDate(date: Date) {
+	const formatted = moment(date)
+	return formatted.locale("id").format("LL")
 }
