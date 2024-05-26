835d0a82208958bcbf1d6c7b9e52d1023fc10384
srtfk <syahrulataufikaja11@gmail.com>
2024-05-26 17:23:22 +0700

    feat: change locale language to english

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -1,16 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import moment from "moment"
-import "moment/locale/id"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 export function localDate(date: Date) {
 	const formatted = moment(date)
-	return formatted.locale("id").format("LL")
+	return formatted.format("LL")
 }
 export function localMonthYear(date: Date) {
 	const formatted = moment(date)
-	return formatted.locale("id").format("MMMM YYYY")
+	return formatted.format("MMMM YYYY")
 }
